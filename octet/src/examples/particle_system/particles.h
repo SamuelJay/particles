940a35c0f5781a0cf7cc92b137ced9f477edd518
SamuelJay <sjpiggott93@gmail.com>
2013-04-16 13:38:50 +0100

    Now in 3D!! Bit choppy when number of particles is much beyond 1000 tho....

--- octet/src/examples/particle_system/particles.h
+++ octet/src/examples/particle_system/particles.h
@@ -8,7 +8,9 @@ public:
    
 private:
   int max_distance_squared;
-  #define NUM_PARTICLES 3
+   enum {
+    NUM_PARTICLES=4000
+   };
   //where each particle is (might be a less expensive way to do this)
   mat4 model_to_projection[NUM_PARTICLES]; 
   mat4 model_to_world[NUM_PARTICLES];
@@ -16,7 +18,7 @@ private:
   float half_height;
   int texture;
   bool enabled[NUM_PARTICLES];
-  float distance;
+  float distance_squared;
   vec4 emitter;
   vec4 distance_vector;
   // the position is used to calculate the distance between the particles and the emitter
@@ -38,11 +40,23 @@ void particles::init(int _texture, float x, float y, float w, float h) {
     //set starting position (will become an emitter)
     positions[i]= emitter;
     //set starting velocity
-    float j;
-    velocities[i]= vec4(0, j = randomizer.get(0.0f, 0.5f), 0, 0);
+    float rand_x;
+    float rand_y;
+    float rand_z;
+    velocities[i]= vec4(rand_x = randomizer.get(-0.5f, 0.5f), rand_y = randomizer.get(-0.5f, 0.5f), rand_z= randomizer.get(-0.5f, 0.5f), 0);
     model_to_world[i].loadIdentity();
-    model_to_world[i].translate(emitter.x(), emitter.y(), 0);
+    model_to_world[i].translate(emitter.x(), emitter.y(), emitter.z());
     enabled[i] = true;
+    
+    /*printf("\nmodel to world starts:\n");
+    printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+    model_to_world[i][0][0] , model_to_world[i][0][1], model_to_world[i][0][2], model_to_world[i][0][3]);
+    printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+    model_to_world[i][1][0] ,model_to_world[i][1][1], model_to_world[i][1][2], model_to_world[i][1][3]);
+    printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+    model_to_world[i][2][0] , model_to_world[i][2][1], model_to_world[i][2][2], model_to_world[i][2][3]);
+    printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+    model_to_world[i][3][0] ,model_to_world[i][3][1], model_to_world[i][3][2], model_to_world[i][3][3]);*/
   }
   texture = _texture;
   //same size and texture for all particles
@@ -54,14 +68,24 @@ void particles::move() {
   for (int i = 0; i < NUM_PARTICLES; i++) { 
 
     positions[i]+=velocities[i];
-    model_to_world[i].translate(velocities[i].x(), velocities[i].y(), 0);
+    model_to_world[i].translate(velocities[i].x(), velocities[i].y(), velocities[i].z());
 
-    distance = (positions[i].x() * positions[i].x()) + (positions[i].y() * positions[i].y());
+    distance_squared = (positions[i].x() * positions[i].x()) + (positions[i].y() * positions[i].y()) + (positions[i].z() * positions[i].z());
 
-    if(distance>max_distance_squared) {
+    if(distance_squared>max_distance_squared) {
       model_to_world[i].loadIdentity();
-      model_to_world[i].translate(emitter.x(), emitter.y(), 0);
+      model_to_world[i].translate(emitter.x(), emitter.y(), emitter.z());
       positions[i] = emitter;
+      
+     /* printf("\nmodel to world out:\n");
+      printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+      model_to_world[i][0][0] , model_to_world[i][0][1], model_to_world[i][0][2], model_to_world[i][0][3]);
+      printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+      model_to_world[i][1][0] ,model_to_world[i][1][1], model_to_world[i][1][2], model_to_world[i][1][3]);
+      printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+      model_to_world[i][2][0] , model_to_world[i][2][1], model_to_world[i][2][2], model_to_world[i][2][3]);
+      printf("0X  : %d  0X  : %d  0Z  :  %d\n",
+      model_to_world[i][3][0] ,model_to_world[i][3][1], model_to_world[i][3][2], model_to_world[i][3][3]);*/
     }
 
   }
